6988d83272629c4fbe7e1c174238c7ff68bedfc3
Travis <82448667+TravisOnline@users.noreply.github.com>
2022-01-19 09:29:36 +0000

    Add files via upload

---
+++ day02.py
@@ -0,0 +1,31 @@
+#Part One
+lines = []
+
+with open('day02_input.txt') as f:
+    lines = f.readlines()
+
+x_pos = 0;
+y_pos = 0;
+aim = 0;
+
+for line in lines:
+    #Read the first letter of the line to get direction
+    #Seperate Lines into Sub strings to get movement aomunt
+    dir = str(line[0])
+    dir_chunks = line.split(' ')
+    amount = int(dir_chunks[1])
+
+    if(dir == "f"):
+        x_pos = x_pos + amount
+        y_pos = y_pos + (aim * amount)
+    elif(dir == "u"):
+        #Part 1 calculation
+        #y_pos = y_pos - amount
+        aim = aim - amount;
+    elif(dir == "d"):
+        #Part 1 calculation
+        #y_pos = y_pos + amount
+        aim = aim + amount;
+
+my_position = x_pos * y_pos
+print(my_position)
